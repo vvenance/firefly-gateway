f1fcdf79c67e72f82fc14539258a2654a51fa57f
Valériane Venance <valeriane.venance@hotmail.fr>
2020-08-05 14:06:59 +0200

    Update elastic-apm-node.js

--- elastic-apm-node.js
+++ elastic-apm-node.js
@@ -1,5 +1,5 @@
 module.exports = {
-    serviceName: process.env.STORE_APP_NAME,
+    serviceName: process.env.SERVICE_NAME,
     serviceNodeName : process.env.INSTANCE_ID,
     serviceVersion : process.env.COMMIT_ID,
     secretToken: process.env.ES_ADDON_APM_AUTH_TOKEN || "",
